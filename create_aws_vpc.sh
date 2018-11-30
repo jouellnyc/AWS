f3059ae40152a44eda3541456eacb4448715c222
jouellnyc <jouellnyc@gmail.com>
2018-11-30 12:42:42 -0500

    Final until LB

--- create_aws_vpc.sh
+++ create_aws_vpc.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash 
 
 ########################################################
 # create_aws_vpc.sh - https://github.com/jouellnyc/AWS #
@@ -11,7 +11,7 @@ export AZ1=us-west-2a
 export AZ2=us-west-2b
 
 #VPC
-export VPCLABEL="PROD-VPC5"
+export VPCLABEL="PROD-VPC2"
 export VPCCIDR="10.0.0.0/16"
 export VPCID=$(aws ec2 create-vpc --cidr-block 10.0.0.0/16 --output text  | grep -ioE "vpc-[A-Za-z0-9]{10,25}")
 aws ec2 create-tags --resources $VPCID --tags Key=Name,Value=$VPCLABEL
@@ -20,7 +20,7 @@ aws ec2 create-tags --resources $VPCID --tags Key=Name,Value=$VPCLABEL
 export SNCIDR1="10.0.1.0/24"
 export SNCIDR2="10.0.2.0/24"
 aws ec2 create-subnet --vpc-id $VPCID --cidr-block $SNCIDR1 --availability-zone  $AZ1
-aws ec2 create-subnet --vpc-id $VPCID --cidr-block $SNCIDR2 --availability-zone  $AZ1
+aws ec2 create-subnet --vpc-id $VPCID --cidr-block $SNCIDR2 --availability-zone  $AZ2
 
 #MYIP
 export MYIP="104.162.77.49"
@@ -64,6 +64,10 @@ export AMI="ami-01bbe152bf19d0289"
 export USERDATA="user_data.http.sh"
 [ -f $USERDATA ] || { echo "No user data"; exit 55; } 
 
-aws ec2 run-instances --image-id $AMI  --count 1 --instance-type $TYPE --key-name $KEYPAIR --security-group-ids $SSH_SG $HTTP_SG --subnet-id $SUBNET1 --user-data file://$USERDATA
-aws ec2 run-instances --image-id $AMI  --count 1 --instance-type $TYPE --key-name $KEYPAIR --security-group-ids $SSH_SG $HTTP_SG --subnet-id $SUBNET2 --user-data file://$USERDATA
-
+aws ec2 run-instances --image-id $AMI  --count 1 --instance-type $TYPE --key-name $KEYPAIR --security-group-ids $SSH_SG $HTTP_SG --subnet-id $SUBNET1 --user-data file://$USERDATA && \
+aws ec2 run-instances --image-id $AMI  --count 1 --instance-type $TYPE --key-name $KEYPAIR --security-group-ids $SSH_SG $HTTP_SG --subnet-id $SUBNET2 --user-data file://$USERDATA && \
+while read -r IP1 IP2; do 
+	echo "== Wait 2 mintutes and then check: ==";
+       	echo "http://$IP1/"
+       	echo "http://$IP2/"
+done < <(aws ec2 describe-instances  --filters "Name=vpc-id,Values=$VPCID"  --query 'Reservations[*].Instances[0].PublicIpAddress' --output text)
