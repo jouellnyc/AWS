a01226f1fbcf760e13daec8ee9a8724d439034bd
jouellnyc <jouellnyc@gmail.com>
2018-12-04 17:43:36 -0500

    initial commit

---
+++ create_aws_alb/create_aws_elbv2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -ue 
+
+export LB_NAME="My-Web-load-balancer"
+export LB_TGT_NAME="My-Web-targets"
+aws elbv2 create-load-balancer --name $LB_NAME --subnets $SUBNET1 $SUBNET2  --security-groups  $LBFROMMYIP
+aws elbv2 create-target-group  --name $LB_TGT_NAME --protocol HTTP --port 80 --vpc-id $VPCID
+export LB_ARN=$(aws elbv2  describe-load-balancers --name $LB_NAME --query 'LoadBalancers[0].{Arn:LoadBalancerArn}' --output text)
+export TGT_ARN=$(aws elbv2  describe-target-groups --query 'TargetGroups[?TargetGroupName==`'$LB_TGT_NAME'`].{ARN:TargetGroupArn}' --output text)
+
+export INSTANCE1=$(aws ec2 describe-instances  --filters "Name=vpc-id,Values=$VPCID" --query 'Reservations[0].Instances[0].InstanceId' --output text)
+export INSTANCE2=$(aws ec2 describe-instances  --filters "Name=vpc-id,Values=$VPCID" --query 'Reservations[1].Instances[0].InstanceId' --output text)
+
+aws elbv2 register-targets --target-group-arn  $TGT_ARN --targets Id=$INSTANCE1 Id=$INSTANCE2
+aws elbv2 create-listener  --load-balancer-arn $LB_ARN --protocol HTTP --port 80 --default-actions Type=forward,TargetGroupArn=$TGT_ARN
+aws elbv2 describe-target-health --target-group-arn  $TGT_ARN
+
+
+
+
