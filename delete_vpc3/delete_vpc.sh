fe95732b99696c902c5228934c3b1c6159605a8b
jouellnyc <jouellnyc@gmail.com>
2018-12-18 17:28:51 -0500

    update

--- delete_vpc3/delete_vpc.sh
+++ delete_vpc3/delete_vpc.sh
@@ -1,6 +1,8 @@
 #If you've run the create scripts via 'source $script', your shell has all the variable needed to run:
 aws ec2 delete-nat-gateway --nat-gateway-id  $NATGW1
 aws ec2 delete-nat-gateway --nat-gateway-id  $NATGW2
+echo "Waiting for 30 seconds"
+sleep 30 
 aws ec2 terminate-instances --instance-ids $INSTANCE1
 aws ec2 terminate-instances --instance-ids $INSTANCE2
 aws elbv2 delete-load-balancer --load-balancer-arn $LB_ARN
