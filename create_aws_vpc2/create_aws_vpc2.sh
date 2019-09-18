7791458a3d499fddc51250d38310cea4ab4d8261
jouellnyc <jouellnyc@gmail.com>
2019-09-17 21:59:38 -0400

    Add verbose output

--- create_aws_vpc2/create_aws_vpc2.sh
+++ create_aws_vpc2/create_aws_vpc2.sh
@@ -16,34 +16,38 @@ export AZ2=us-west-2b
 #VPC
 export VPCLABEL="PROD-VPC"
 export VPCCIDR="10.0.0.0/16"
+echo "Creating VPC $VPCLABEL" 
 export VPCID=$(aws ec2 create-vpc --cidr-block 10.0.0.0/16 --output text  | grep -ioE "vpc-[A-Za-z0-9]{10,25}")
-aws ec2 create-tags --resources $VPCID --tags Key=Name,Value=$VPCLABEL
+aws ec2 create-tags --resources $VPCID --tags Key=Name,Value=$VPCLABEL && echo "VPC and Vpc Tags created OK" 
 
 #SUBNETS
 export SNCIDR1="10.0.1.0/24"
 export SNCIDR2="10.0.2.0/24"
-aws ec2 create-subnet --vpc-id $VPCID --cidr-block $SNCIDR1 --availability-zone  $AZ1
-aws ec2 create-subnet --vpc-id $VPCID --cidr-block $SNCIDR2 --availability-zone  $AZ2
+aws ec2 create-subnet --vpc-id $VPCID --cidr-block $SNCIDR1 --availability-zone  $AZ1 && echo "Subnet 1 created OK"
+aws ec2 create-subnet --vpc-id $VPCID --cidr-block $SNCIDR2 --availability-zone  $AZ2 && echo "Subnet 2 created OK"
 
 ############No Need to touch below ###########
 sleep 2
 export SUBNET1=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$VPCID" "Name=cidr,Values=$SNCIDR1" --query 'Subnets[*].{ID:SubnetId}' --output text)
 export SUBNET2=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$VPCID" "Name=cidr,Values=$SNCIDR2" --query 'Subnets[*].{ID:SubnetId}' --output text)
-aws ec2 create-tags --resources $SUBNET1  --tags Key=Name,Value=subnet-"${AZ1}"
-aws ec2 create-tags --resources $SUBNET2  --tags Key=Name,Value=subnet-"${AZ2}"
-aws ec2 modify-subnet-attribute --subnet-id  $SUBNET1 --map-public-ip-on-launch
-aws ec2 modify-subnet-attribute --subnet-id  $SUBNET2 --map-public-ip-on-launch
+aws ec2 create-tags --resources $SUBNET1  --tags Key=Name,Value=subnet-"${AZ1}" && \
+aws ec2 create-tags --resources $SUBNET2  --tags Key=Name,Value=subnet-"${AZ2}" && \
+aws ec2 modify-subnet-attribute --subnet-id  $SUBNET1 --map-public-ip-on-launch && \
+aws ec2 modify-subnet-attribute --subnet-id  $SUBNET2 --map-public-ip-on-launch && \
+echo "Subnet Tags and Attributes set OK"
 
 #IGW
-export IGWID=$(aws ec2 create-internet-gateway --output text  | awk  '{ print $2 }')
-aws ec2 attach-internet-gateway --vpc-id $VPCID   --internet-gateway-id $IGWID 
+export IGWID=$(aws ec2 create-internet-gateway --output text  | awk  '{ print $2 }') && \
+aws ec2 attach-internet-gateway --vpc-id $VPCID   --internet-gateway-id $IGWID && \
+echo "Internet Gateway Created and attached OK"
 
 #ROUTE TABLE
 #aws ec1 create-route-table --vpc-id $VPCID
 export RTID=$(aws ec2 describe-route-tables --filters "Name=vpc-id,Values=$VPCID" --query "RouteTables[].RouteTableId[]" --output text)
-aws ec2 create-route --route-table-id $RTID  --destination-cidr-block 0.0.0.0/0 --gateway-id $IGWID 
-aws ec2 associate-route-table  --subnet-id $SUBNET1 --route-table-id $RTID
-aws ec2 associate-route-table  --subnet-id $SUBNET2 --route-table-id $RTID
+aws ec2 create-route --route-table-id $RTID  --destination-cidr-block 0.0.0.0/0 --gateway-id $IGWID && \
+aws ec2 associate-route-table  --subnet-id $SUBNET1 --route-table-id $RTID && \
+aws ec2 associate-route-table  --subnet-id $SUBNET2 --route-table-id $RTID && \
+echo "Routes and Route Tables setup OK"
 
 #KEY PAIRS
 #Caution!
@@ -52,22 +56,26 @@ export KEYPAIR="${VPCLABEL}-key.pem"
 # Caution!
 aws ec2 delete-key-pair --key-name $KEYPAIR
 [ -f $KEYPAIR ] && rm -f $KEYPAIR
-aws ec2 create-key-pair --key-name $KEYPAIR --query 'KeyMaterial' --output text > $KEYPAIR
-chmod 400 $KEYPAIR
+aws ec2 create-key-pair --key-name $KEYPAIR --query 'KeyMaterial' --output text > $KEYPAIR && \
+chmod 400 $KEYPAIR && \
+echo "Key Pairs created OK"
 
 ###SECURITY GROUPS
-export LBFROMMYIP=$(aws ec2 create-security-group --group-name LBFROMMYIP --description "LBfromMYIP" --vpc-id $VPCID --output text)
-export LBFROMEC2S=$(aws ec2 create-security-group --group-name LBFROMEC2S --description "LBfromEC2s" --vpc-id $VPCID --output text)
-export EC2FROMLB=$(aws ec2 create-security-group --group-name EC2FROMLB --description "EC2fromLB" --vpc-id $VPCID --output text)
+export LBFROMMYIP=$(aws ec2 create-security-group --group-name LBFROMMYIP --description "LBfromMYIP" --vpc-id $VPCID --output text) && \
+export LBFROMEC2S=$(aws ec2 create-security-group --group-name LBFROMEC2S --description "LBfromEC2s" --vpc-id $VPCID --output text) && \
+export EC2FROMLB=$(aws ec2 create-security-group --group-name EC2FROMLB --description "EC2fromLB" --vpc-id $VPCID --output text) &&  \
+echo "Security Groups Created OK"
 
 #Change "--cidr $MYIP/32" to "--cidr 0.0.0.0/0"  to expose to the Internet at large
-aws ec2 authorize-security-group-ingress --group-id $LBFROMMYIP --protocol tcp --port 80 --cidr $MYIP/32
-aws ec2 authorize-security-group-ingress --group-id $EC2FROMLB  --protocol tcp --port 80 --source-group $LBFROMMYIP
-aws ec2 create-tags --resources $LBFROMMYIP --tags Key=Name,Value="LBFROMMYIP"
-aws ec2 create-tags --resources $EC2FROMLB  --tags Key=Name,Value="EC2FROMLB"
+aws ec2 authorize-security-group-ingress --group-id $LBFROMMYIP --protocol tcp --port 80 --cidr $MYIP/32 && \
+aws ec2 authorize-security-group-ingress --group-id $EC2FROMLB  --protocol tcp --port 80 --source-group $LBFROMMYIP && \
+aws ec2 create-tags --resources $LBFROMMYIP --tags Key=Name,Value="LBFROMMYIP" && \
+aws ec2 create-tags --resources $EC2FROMLB  --tags Key=Name,Value="EC2FROMLB"  && \
+echo "Security Groups setup and Tags created OK"
+
 
 #EC2 INSTANCES
 export TYPE="t2.micro"
 export AMI="ami-01bbe152bf19d0289"
 export USERDATA="../user_data.http.sh"
-[ -f $USERDATA ] || { echo "No user data"; exit 55; } 
+[ -f $USERDATA ] && echo "Ami Data Ready for next Step"  || { echo "No user data"; echo exit 55; }  
