33d26f18c3e6729580a7531fa51d4f87760839e5
jouellnyc <jouellnyc@gmail.com>
2020-02-27 18:04:13 -0500

    Dockerize EC2 instances

--- user_data.http.sh
+++ user_data.http.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 yum update -y
-yum -y install httpd
-service httpd start
-chkconfig httpd on
-/usr/bin/hostname > /var/www/html/index.html
+yum -y install docker
+yum -y install git 
+BDIR="/gitrepos/shouldipickitup"
+mkdir -p $BDIR 
+cd $BDIR
+git clone https://github.com/jouellnyc/shouldipickitup.git 
+curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose up -d
+
