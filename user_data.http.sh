ae2d2b5bd9966781ef84fb4f0b0c14b9e2123129
jouellnyc <jouellnyc@gmail.com>
2020-02-27 18:32:26 -0500

    update logic to allow install to happen

--- user_data.http.sh
+++ user_data.http.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
+
 yum update -y
+
 yum -y install docker
 yum -y install git 
-BDIR="/gitrepos/shouldipickitup"
-mkdir -p $BDIR 
-cd $BDIR
-git clone https://github.com/jouellnyc/shouldipickitup.git 
+
 curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
 chmod +x /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-docker-compose up -d
 
+service docker start
+chkconfig docker on
+
+BDIR="/gitrepos/"
+mkdir -p $BDIR 
+cd $BDIR/
+git clone https://github.com/jouellnyc/shouldipickitup.git 
+cd shouldipickitup
+docker-compose up -d
