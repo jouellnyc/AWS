8835f206f6e0fda079194974632036ae6d305029
jouellnyc <jouellnyc@gmail.com>
2020-02-28 14:41:25 -0500

    AWSLOGS

--- create_launch_config/create_launch_config.sh
+++ create_launch_config/create_launch_config.sh
@@ -2,7 +2,7 @@
 
 #### Create an Auto Scaling Launch Configuation ####
 
-export LC_NAME="Auto-Scaling-Launch-Config-Docker-v4"
+export LC_NAME="Auto-Scaling-Launch-Config-Docker-v5"
 export TYPE="t2.micro"
 export AMI="ami-01bbe152bf19d0289"
 export USERDATA="../user_data.http.sh"
@@ -11,9 +11,12 @@ export USERDATA="../user_data.http.sh"
 export SGROUP1="sg-00b4e2e5337079a81"
 export SGROUP2="sg-028813c0505b2ddfb"
 export SGROUP3="sg-0842bd4fffc901c60"
+export INST_PROF="CloudWatchAgentAdminRole"
+
 
 [ -f $USERDATA ] || { echo "No user data"; exit 55; }
 
 aws autoscaling create-launch-configuration --launch-configuration-name $LC_NAME \
 --instance-type $TYPE --key-name "PROD-VPC-key.pem" --security-groups $SGROUP1 \
-$SGROUP2 $SGROUP3 --user-data file://$USERDATA --image-id $AMI && echo "Created AutoScaling Config OK"
+$SGROUP2 $SGROUP3 --iam-instance-profile  $INST_PROF  --user-data file://$USERDATA \
+--image-id $AMI && echo "Created AutoScaling Config OK"
