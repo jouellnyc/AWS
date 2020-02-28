f29ac17c012694226a4898e157e269737fb4ecc2
jouellnyc <jouellnyc@gmail.com>
2020-02-27 21:36:37 -0500

    &&

--- update_auto_scaling/update_auto_scaling.sh
+++ update_auto_scaling/update_auto_scaling.sh
@@ -10,4 +10,4 @@ export ASG_NAME="Auto-Scaling-Group"
 export MIN_SERVERS=$1
 export MAX_SERVERS=$2
 aws autoscaling update-auto-scaling-group --auto-scaling-group-name $ASG_NAME \
---min-size $MIN_SERVERS --max-size $MAX_SERVERS || echo "Appears Successful"
+--min-size $MIN_SERVERS --max-size $MAX_SERVERS &&  echo "Appears Successful"
