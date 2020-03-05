d21df74e8e891642dc095e5e3b2e8482bc095218
jouellnyc <jouellnyc@gmail.com>
2020-03-04 22:37:54 -0500

    Add $1 option

--- update_auto_scaling/update_auto_scaling.sh
+++ update_auto_scaling/update_auto_scaling.sh
@@ -10,8 +10,8 @@ export ASG_NAME=${1:-"Auto-Scaling-Group"}
 #Auto-Scaling-Group-BLUE
 #Auto-Scaling-Group-GREEN
 
-export MIN_SERVERS=$1
-export MAX_SERVERS=$2
+export MIN_SERVERS=$2
+export MAX_SERVERS=$3
 source ../shared_vars.txt >/dev/null 2>&1 || source ./shared_vars.txt
 
 aws autoscaling update-auto-scaling-group --auto-scaling-group-name $ASG_NAME \
