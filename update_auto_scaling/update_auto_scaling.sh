586931f25a2c59cbbaa3ab7391538381c0bc08d5
jouellnyc <jouellnyc@gmail.com>
2020-03-04 22:36:09 -0500

    Add $1 option

--- update_auto_scaling/update_auto_scaling.sh
+++ update_auto_scaling/update_auto_scaling.sh
@@ -6,7 +6,10 @@ set -e
 
 [[ $# -eq 0 ]] && { echo "usage: $0 MIN MAX"; exit 55;  }
 
-export ASG_NAME="Auto-Scaling-Group"
+export ASG_NAME=${1:-"Auto-Scaling-Group"}
+#Auto-Scaling-Group-BLUE
+#Auto-Scaling-Group-GREEN
+
 export MIN_SERVERS=$1
 export MAX_SERVERS=$2
 source ../shared_vars.txt >/dev/null 2>&1 || source ./shared_vars.txt
