0d8b340223d0a12a086837eb33ab50af2e987195
jouellnyc <jouellnyc@gmail.com>
2020-03-06 23:55:06 -0500

    restoring general user data files

--- user_data.http.sh
+++ user_data.http.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
 
 yum update -y
-
-amazon-linux-extras install docker
-yum -y install git 
-yum -y install awslogs 
-
-curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
-chmod +x /usr/local/bin/docker-compose
-ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-
-service docker start
-chkconfig docker on
-service awslogsd start
-chkconfig awslogsd on
-
-BDIR="/gitrepos/"
-mkdir -p $BDIR 
-cd $BDIR/
-git clone https://github.com/jouellnyc/shouldipickitup.git 
-cd shouldipickitup
-docker-compose -f docker-compose.AWS.yaml up -d
+yum -y install httpd
+service httpd start
+chkconfig httpd on
+/usr/bin/hostname > /var/www/html/index.html
