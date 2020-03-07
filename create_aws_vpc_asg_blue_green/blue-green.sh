772d5b32e01d6841ea14f888bb8e9a1c99214a0d
jouellnyc <jouellnyc@gmail.com>
2020-03-06 23:51:09 -0500

    restoring general user data files

--- create_aws_vpc_asg_blue_green/blue-green.sh
+++ create_aws_vpc_asg_blue_green/blue-green.sh
@@ -6,12 +6,12 @@ export PORT1="80"
 export PORT2="443"
 
 # 1. Required - List the name of the Load Balancer
-export LB_NAME="My-Web-Load-Balancer"
+source ../shared_vars.txt
 
 # 2. Required - List the name of the *** Target *** Group Name
 export TG_NAME=$1
-#export TG_NAME="Target-Group-Auto-Scaling-BLUE"
-#export TG_NAME="Target-Group-Auto-Scaling-GREEN"
+#export TG_NAME="Target-GRP-Auto-Scale-GREEN"
+#export TG_NAME="Target-GRP-Auto-Scale-BLUE"
 
 # Do not touch below here #
 
@@ -19,14 +19,14 @@ export LB_ARN=$(aws elbv2  describe-load-balancers --name $LB_NAME --query 'Load
 
 export LST_ARN1=$(aws elbv2 describe-listeners --load-balancer-arn  $LB_ARN \
 --query 'Listeners[?Port==`'$PORT1'`].ListenerArn'  --output text)
-export LST_ARN2=$(aws elbv2 describe-listeners --load-balancer-arn  $LB_ARN \
- --query 'Listeners[?Port==`'$PORT2'`].ListenerArn' --output text)
+#export LST_ARN2=$(aws elbv2 describe-listeners --load-balancer-arn  $LB_ARN \
+# --query 'Listeners[?Port==`'$PORT2'`].ListenerArn' --output text)
 
 export TG_ARN=$(aws elbv2  describe-target-groups --query \
 'TargetGroups[?TargetGroupName==`'$TG_NAME'`].{ARN:TargetGroupArn}' --output text)
 
 aws elbv2 modify-listener --listener-arn $LST_ARN1 --default-actions Type=forward,TargetGroupArn=$TG_ARN
-aws elbv2 modify-listener --listener-arn $LST_ARN2 --default-actions Type=forward,TargetGroupArn=$TG_ARN
+#aws elbv2 modify-listener --listener-arn $LST_ARN2 --default-actions Type=forward,TargetGroupArn=$TG_ARN
 
 
 ###########
