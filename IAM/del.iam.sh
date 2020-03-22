889bde6cad5e5c4beb273f1dbf85607306b1fe7f
jouellnyc <jouellnyc@gmail.com>
2020-03-21 22:52:20 -0400

    Update per AWS

--- IAM/del.iam.sh
+++ IAM/del.iam.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 source ../shared_vars.txt
-aws iam remove-role-from-instance-profile --instance-profile-name $INST_PROF  --role-name $CW_ROLE 
-aws iam delete-instance-profile --instance-profile-name $INST_PROF 
-aws iam delete-role --role-name $CW_ROLE 
 
 POLICY1=$(aws iam list-policies --scope Local --output json  | grep  $AS_POLICY | grep Arn | cut -d \"  -f4)
 POLICY2=$(aws iam list-policies --scope Local --output json  | grep  $CW_POLICY | grep Arn | cut -d \"  -f4)
+aws iam detach-role-policy --role-name $APP_ROLE --policy-arn $POLICY1
+aws iam detach-role-policy --role-name $APP_ROLE --policy-arn $POLICY2
+
 aws iam delete-policy --policy-arn $POLICY1
 aws iam delete-policy --policy-arn $POLICY2
+
+aws iam remove-role-from-instance-profile --instance-profile-name $INST_PROF  --role-name $APP_ROLE 
+aws iam delete-role --role-name $APP_ROLE
+
+aws iam delete-instance-profile --instance-profile-name $INST_PROF 
 
 export WEB_SRV="nginx"
 export WEB_APP="flask"
