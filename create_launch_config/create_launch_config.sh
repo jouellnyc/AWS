3ff3cb997b528db9b3e399b70f3c5e6c092f27d3
jouellnyc <jouellnyc@gmail.com>
2020-03-25 14:46:55 -0400

    One Click Deploy!

--- create_launch_config/create_launch_config.sh
+++ create_launch_config/create_launch_config.sh
@@ -2,15 +2,14 @@
 
 #### Create an Auto Scaling Launch Configuation ####
 
+export LC_NAME="Auto-Scaling-Launch-Config-Docker-v11"
+
 source ../shared_vars.txt  >/dev/null 2>&1  || source ./shared_vars.txt 
 
-export USERDATA="../user_data.http.sh"
+export USERDATA="/home/john/gitrepos/shouldipickitup/user_data.http.AWS.sh"
 [ -f $USERDATA ] || { echo "No user data"; exit 55; }
 
-export LC_NAME="Auto-Scaling-Launch-Config-Docker-v2"
-export KEY_NAME="vpc-0fb0808e945bfc5f5-key.pem"
-
 aws autoscaling create-launch-configuration --launch-configuration-name $LC_NAME \
---instance-type $TYPE --key-name $KEY_NAME --security-groups $SGROUP1 \
-$SGROUP2 $SGROUP3 --iam-instance-profile  $INST_PROF  --user-data file://$USERDATA \
+--instance-type $TYPE --key-name $KEYPAIR --security-groups $EC2FROMLB \
+$LBFROMEC2S $LBFROMMYIP $SSH --iam-instance-profile  $INST_PROF  --user-data file://$USERDATA \
 --image-id $AMI && echo "Created AutoScaling Config OK"
