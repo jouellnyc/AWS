efa192ece0015ba31dbc63dc40172c4c8d53edc9
jouellnyc <jouellnyc@gmail.com>
2020-03-25 22:27:49 -0400

    One Click Deploy!

--- create_aws_vpc_asg_blue_green/create_aws_vpc3.sh
+++ create_aws_vpc_asg_blue_green/create_aws_vpc3.sh
@@ -5,7 +5,13 @@
 ################################################################################
 
 if [ -z "$DEFAULT_AWS_PROFILE" -a -z "$AWS_PROFILE" ];then
-   echo "DEFAULT_AWS_PROFILE and AWS_PROFILE are  not set"
+   echo "DEFAULT_AWS_PROFILE and AWS_PROFILE are not set"
+   exit
+fi
+
+if [ ! "$DEFAULT_AWS_PROFILE" =  "$AWS_PROFILE" ];then
+   echo "DEFAULT_AWS_PROFILE and AWS_PROFILE do not match!"
+   echo "$DEFAULT_AWS_PROFILE and $AWS_PROFILE"
    exit
 fi
 
