62ed829b7e12f48209aef2a614028ca3e0e86834
jouellnyc <jouellnyc@gmail.com>
2021-01-10 22:46:51 -0500

    +location changes

--- boto3/blue_green_deploy/fd.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-
-from alpha_vantage.fundamentaldata import FundamentalData
-
-fd = FundamentalData(key="XCLK59G68DQR3EFM", output_format="pandas")
-
-print(fd.get_income_statement_annual("LMND"))
