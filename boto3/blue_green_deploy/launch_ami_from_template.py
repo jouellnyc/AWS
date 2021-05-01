9361c91a6fefd1ab0e9f7540e7cdd8eeb6cb49cc
jouellnyc <jouellnyc@gmail.com>
2021-04-30 23:29:02 -0400

    ++Launch Template Jsons

--- boto3/blue_green_deploy/launch_ami_from_template.py
+++ boto3/blue_green_deploy/launch_ami_from_template.py
@@ -6,16 +6,20 @@ from aws_cred_objects import AWS_CREDS
 
 aws = AWS_CREDS(aws_profile)
 
-def launch(name, version):
+def launch(name, version, max=1, min=1):
     
         aws.ec2_res.create_instances(
         LaunchTemplate={"LaunchTemplateName": name, "Version": str(version) },
-        MinCount=1,
-        MaxCount=1,
+        MinCount=min,
+        MaxCount=max,
         SubnetId="subnet-0b044a7ae98bb2b00",
     )
     
     
 if __name__ == '__main__':
     
-    launch("Flywheel",4)
+    
+    launch("Flywheel", 6)
+    launch("Crawler", 1)
+    
+    
